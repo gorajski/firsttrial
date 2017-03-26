6ffd7dc4174fa163e74ad8de900a6268c90da3a8
gorajski <nickgorajski@gmail.com>
2017-03-26 16:35:21 -0500

    Initial commit

---
+++ src/main/java/com/gorajski/FirsttrialApplication.java
@@ -0,0 +1,12 @@
+package com.gorajski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsttrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirsttrialApplication.class, args);
+	}
+}
